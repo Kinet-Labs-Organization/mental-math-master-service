c92ee272723d61c9b96af0db0ac1ec165749be38
gitsumanmandal <gitsumanmandal>
2025-12-16 23:36:49 +0530

    first commit

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { GetUser } from "@/src/auth/decorator";
 import { JwtGuard } from "@/src/auth/guard";
@@ -7,10 +7,49 @@ import { AccessTokenDto } from "@/src/auth/dto";
 @UseGuards(JwtGuard)
 @Controller("user")
 export class UserController {
-  constructor(private readonly UserService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Get("me")
   async me(@GetUser() user: AccessTokenDto) {
     return user;
+  }
+
+  ////
+
+  @UseGuards(JwtGuard)
+  @Get('progressReports')
+  async progressReports() {
+    return this.userService.progressReports();
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('practiceGames')
+  async practiceGames() {
+    return this.userService.practiceGames();
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('tournamentGames')
+  async tournamentGames() {
+    return this.userService.tournamentGames();
+  }
+
+  // @Post('login')
+  // async login(
+  //   @Body() { email, password }: { email: string; password: string },
+  // ) {
+  //   return this.userService.login({ email, password });
+  // }
+
+  // @UseGuards(JwtGuard)
+  // @Get('me')
+  // async me(@Request() req: any) {
+  //   return req.user;
+  // }
+
+  @UseGuards(JwtGuard)
+  @Get('flashGame/:id')
+  async flashGame(@Param('id') id: string) {
+    return this.userService.flashGame(id);
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -5,6 +5,7 @@ import {
 } from "@nestjs/common";
 import { Prisma, User } from "@prisma/client";
 import { PrismaService } from "../../database/prisma/prisma.service";
+import { USERS, REPORT, PRACTICE_GAMES, TOURNAMENT_GAMES, FLASH_GAMES, MOCK_API_DELAY} from "@/src/utils/mock";
 
 @Injectable()
 export class UserService {
@@ -28,5 +29,61 @@ export class UserService {
       throw new UnprocessableEntityException("Email already exists");
     }
     return this.prisma.user.create({ data });
+  }
+
+  ////
+
+  // async login({ email, password }: { email: string; password: string }) {
+  //   const user = USERS.find(
+  //     (user) => user.email === email && user.password === password,
+  //   );
+  //   if (!user) {
+  //     throw new Error('Invalid credentials');
+  //   }
+  //   await new Promise((resolve) =>
+  //     setTimeout(() => resolve(1), MOCK_API_DELAY),
+  //   );
+  //   return this.authService.login(user);
+  // }
+
+  async progressReports() {
+    const response = await new Promise((resolve) =>
+      setTimeout(() => resolve(REPORT), MOCK_API_DELAY),
+    );
+    return response;
+  }
+
+  async practiceGames() {
+    const response = await new Promise((resolve) =>
+      setTimeout(() => resolve(PRACTICE_GAMES), MOCK_API_DELAY),
+    );
+    return response;
+  }
+
+  async tournamentGames() {
+    const response = await new Promise((resolve) =>
+      setTimeout(() => resolve(TOURNAMENT_GAMES), MOCK_API_DELAY),
+    );
+    return response;
+  }
+
+  async flashGame(id: string) {
+    const game: any = FLASH_GAMES.find((game) => game.id === id);
+
+    const min = Math.pow(10, game.digitCount - 1);
+    const max = Math.pow(10, game.digitCount) - 1;
+    const newNumbers: any[] = [];
+
+    for (let i = 0; i < game.numberCount; i++) {
+      const value = Math.floor(Math.random() * (max - min + 1)) + min;
+      const operation =
+        game.operations[Math.floor(Math.random() * game.operations.length)];
+      newNumbers.push({ value, operation });
+    }
+
+    const response = await new Promise((resolve) =>
+      setTimeout(() => resolve(newNumbers), MOCK_API_DELAY),
+    );
+    return response;
   }
 }
